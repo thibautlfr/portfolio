cd8ae1c204450aac6a0e07cc64edc004fbe002d0
thibautlfr <thibaut.lfr@gmail.com>
2026-06-12 18:25:22 +0200

    feat: update project configuration and enhance ProjectCard component with cover image support

--- src/config.ts
+++ src/config.ts
@@ -8,6 +8,7 @@ export const navLinks = [
 
 export const socials = [
   { label: "GitHub", href: "https://github.com/thibautlfr" },
-  { label: "Twitter", href: "https://x.com/thibautlfr" },
   { label: "LinkedIn", href: "https://linkedin.com/in/thibaut-lefrancois" },
+  { label: "Figma", href: "https://www.figma.com/@thibautlfr" },
+  { label: "Twitter", href: "https://x.com/thibautlfr" },
 ] as const;
